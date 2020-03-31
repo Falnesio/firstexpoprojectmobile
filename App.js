a42bc53830ccf7dca41599ccf7fb61ba305e7ba2
Falnesio <falnesio.ghander@economia.ufjf.br>
2020-03-31 02:11:02 -0300

    Iniciar criação de app mobile
    Signed-off-by: Falnesio <falnesio.ghander@economia.ufjf.br>

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello Fal!</Text>
+      <Text style={styles.title}>Hello Fal!</Text>
+      <Text style={styles.title}>Hello Fal!</Text>
+      <Text style={styles.title}>Hello Fal!</Text>
+      <Text style={styles.title}>Hello Fal!</Text>
+      <Text style={styles.title}>Hello Fal!</Text>
+      <Text style={styles.title}>Hello Fal!</Text>
+      <Text style={styles.title}>Hello Fal!</Text>
+      <Text style={styles.title}>Hello Fal!</Text>
+      <Text style={styles.title}>Hello Fal!</Text>
+      <Text style={styles.title}>Hello Fal!</Text>
+      <Text style={styles.title}>Hello Fal!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  title: {
+    color: '#FFF',
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
+});
