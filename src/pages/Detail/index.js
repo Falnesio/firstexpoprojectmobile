e0e9ed4589d74b0cec9d56a1d1a25e86a18eb526
Falnesio <falnesio.ghander@economia.ufjf.br>
2020-04-02 02:43:31 -0300

    corrigir formatação do texto e adicionar dados da base de dados
    Signed-off-by: Falnesio <falnesio.ghander@economia.ufjf.br>

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -47,8 +47,8 @@ export default function Detail() {
             </View>
 
             <View style={styles.incident}>
-                <Text style={styles.incidentProperty}>ONG:</Text>
-                <Text style={styles.incidentValue}>{incident.name}</Text>
+                <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
+                <Text style={styles.incidentValue}>{incident.name} de {incident.city}/{incident.uf}</Text>
 
                 <Text style={styles.incidentProperty}>Caso:</Text>
                 <Text style={styles.incidentValue}>{incident.title}</Text>
