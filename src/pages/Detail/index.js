c43be7829aefe049d84771f1dd74f09da0ca9525
Falnesio <falnesio.ghander@economia.ufjf.br>
2020-04-02 02:33:00 -0300

    Details - criar navegação e links para whatsapp e email         - implementar formatação correta de moeda
    Signed-off-by: Falnesio <falnesio.ghander@economia.ufjf.br>

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,18 +1,43 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
-import {View, Text, Image, TouchableOpacity} from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import {View, Text, Image, TouchableOpacity, Linking} from 'react-native';
+import * as MailCompose from 'expo-mail-composer';
 
 import logoImg from '../../assets/logo.png';
 
 import styles from "../Detail/styles";
 
 export default function Detail() {
+    const navigation = useNavigation();
+    const route = useRoute();
+
+    const incident = route.params.incident;
+
+    const message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+    function navigateBack() {
+        navigation.goBack()
+    }
+
+    function sendMail() {
+        MailCompose.composeAsync({
+            subject: 'Herói do Caso: Nome do Caso',
+            recipients: ['falnesio.ghander@economia.ufjf.br'],
+            body: message,
+        })
+    }
+
+    function sendWhatsapp() {
+        Linking.openURL(`whatsapp://send?phone=33920005046Gtext=${message}`);
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={logoImg} />
 
-                <TouchableOpacity onPress={() => {}}>
+                <TouchableOpacity onPress={navigateBack}>
                     <Feather
                       name="arrow-left"
                       size={28}
@@ -22,14 +47,19 @@ export default function Detail() {
             </View>
 
             <View style={styles.incident}>
-                <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
-                <Text style={styles.incidentValue}>APAD</Text>
+                <Text style={styles.incidentProperty}>ONG:</Text>
+                <Text style={styles.incidentValue}>{incident.name}</Text>
 
                 <Text style={styles.incidentProperty}>Caso:</Text>
-                <Text style={styles.incidentValue}>Cadeira</Text>
+                <Text style={styles.incidentValue}>{incident.title}</Text>
 
                 <Text style={styles.incidentProperty}>Valor:</Text>
-                <Text style={styles.incidentValue}>$R120,00</Text>
+                <Text style={styles.incidentValue}>
+                    {Intl.NumberFormat('pt-BR', {
+                      style: 'currency',
+                      currency: 'BRL'
+                    }).format(incident.value)}
+                </Text>
             </View>
 
             <View style={styles.contactBox}>
@@ -39,11 +69,11 @@ export default function Detail() {
                 <Text style={styles.heroDescription}>Entre em contato:</Text>
 
                 <View style={styles.actions}>
-                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                    <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
                         <Text style={styles.actionText}>Whatsapp</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                    <TouchableOpacity style={styles.action} onPress={sendMail}>
                         <Text style={styles.actionText}>E-mail</Text>
                     </TouchableOpacity>
                 </View>
